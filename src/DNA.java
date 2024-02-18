444c0653b7440a6927b805a7f8afed60779ced06
kizuna <dy79.untalan@gmail.com>
2024-02-17 20:41:36 -0800

    final

--- src/DNA.java
+++ src/DNA.java
@@ -1,11 +1,15 @@
 public class DNA {
+    // Property initialization; array of object NucleicAcid.
     private NucleicAcid[] LtoRHelix;
     private NucleicAcid[] RtoLHelix;
 
+    // Default constructor initialization.
     public DNA() {
 
     }
 
+    // Overloaded constructor containing the LtoRHelixPopulate method and code
+    // to populate the RtoLHelix.
     public DNA(String strand) {
         LtoRHelixPopulate(strand);
 
@@ -38,10 +42,12 @@ public class DNA {
     }
 
 
+    // Method that utilizes the strand pulled via the DNADriver class.
     public void LtoRHelixPopulate(String strand) {
         this.LtoRHelix = new NucleicAcid[strand.length()];
 
-
+        // A for-loop which parses the nucleic acids within and of the length of the strand;
+        // utilizing a switch statement  to  assign the properties of the nucleic acids found in DNA and RNA.
         for (int i = 0; i < strand.length(); i++) {
             char currAcid = strand.charAt(i);
 
@@ -68,6 +74,7 @@ public class DNA {
         }
     }
 
+    // Print method that prints out the properties of the nucleotides in both helical arrays.
     public void print() {
         System.out.println("[!] - Printing: Left to Right Helix Nucleotide Properties...");
         for (int i = 0; i < LtoRHelix.length; i++) {
@@ -84,17 +91,80 @@ public class DNA {
         System.out.println("[!] - Completed!");
     }
 
-   /*public void highestMolarMass(){
-        System.out.println("[!] - Printing: Indice )
-        if(currMolarMass > highestMolarMass){
-            highestMolarMass = currMolarMass;
-            highestIndex=i;
-        }
-    }
-    */
-    public void totalDensity(){
-        for (int i = 0; i < LtoRHelix.length; i++){
+    // Print method which parses through both helical arrays and determines the highest molar mass
+    // at every index within the arrays, then prints out the first occurrence of the highest mass.
+    public void highestMolarMass() {
+        System.out.println("[!] - Printing: Molar Mass & Indices... ");
+        int highestIndex = -1;
+        float highestMass = 0;
 
+        System.out.println("[!] - Printing: LtoR Helix Masses & Indices...");
+
+
+        for (int i = 0; i < LtoRHelix.length; i++) {
+            NucleicAcid lrMass = LtoRHelix[i];
+            float mass = lrMass.getMolarMass();
+
+            if (mass > highestMass) {
+                highestMass = mass;
+                highestIndex = 1;
+            }
+
+            System.out.println("The highest molar mass in the Left to Right helix at " + i + "  is... " + highestMass);
         }
+
+        if (highestIndex != -1) { // Check if a higher mass was found
+            System.out.println("The highest molar mass in the Left to Right helix is " + highestMass +
+                    " at index " + highestIndex);
+        }
+
+        System.out.println("[!] - Completed!");
+
+        System.out.println("[!] - Printing: RtoL Helix Masses & Indices...");
+
+        for (int i = 0; i < RtoLHelix.length; i++) {
+            NucleicAcid rlMass = RtoLHelix[i];
+            float mass = rlMass.getMolarMass();
+
+            if (mass > highestMass) {
+                highestMass = mass;
+                highestIndex = 1;
+            }
+
+            System.out.println("The highest molar mass in the Right to Left helix at " + i + "  is... " + highestMass);
+        }
+
+        if (highestIndex != -1) { // Check if a higher mass was found
+            System.out.println("The highest molar mass in the Right to Left helix is " + highestMass +
+                    " at index " + highestIndex);
+        }
+
+        System.out.println("[!] - Completed!");
+    }
+
+    // Final print method which summates the densities of all indices in each helical array.
+    public void totalDensity(){
+        System.out.println("[!] - Printing: Left to Right Helix Density...");
+
+        float totalDensity = 0;
+        for (int i = 0; i < LtoRHelix.length; i++) {
+            NucleicAcid lrDens = LtoRHelix[i];
+            float density = lrDens.getDensity();
+
+            totalDensity += density;
+        }
+
+        System.out.println("[*] - The total density from left to right is... " + totalDensity + " g/cm3.");
+
+        System.out.println("[!] - Printing: Right to Left Helix Density...");
+
+        for (int i = 0; i < RtoLHelix.length; i++) {
+            NucleicAcid rlDens = RtoLHelix[i];
+            float density = rlDens.getDensity();
+
+            totalDensity += density;
+        }
+
+        System.out.println("[*] - The total density from right to left is... " + totalDensity + " g/cm3.");
     }
 }
--- src/DNADriver.java
+++ src/DNADriver.java
@@ -2,11 +2,17 @@ import java.util.Scanner;
 public class DNADriver {
     public static void main(String[] args) {
         Scanner uI = new Scanner(System.in);
-        System.out.println("[?] - Please enter a sequence of nucleotides: ");
+        System.out.print("[?] - Please enter a sequence of nucleotides: ");
         DNA a1 = new DNA(uI.next());
         uI.close();
 
         System.out.println("[!] - Printing Helix Properties...");
         a1.print();
+
+        System.out.println("[!] - Printing Highest Total Molar Mass...");
+        a1.highestMolarMass();
+
+        System.out.println("[!] - Printing Total Density...");
+        a1.totalDensity();
     }
 }
